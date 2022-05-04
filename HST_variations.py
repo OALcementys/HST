2864859f5b246098b128e49f940c3be1589cfe2a
otmane alami <otmanealami@cementys.com>
2022-05-04 09:25:51 +0200

    final code

---
+++ HST_variations.py
@@ -0,0 +1,472 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[142]:
+
+
+import numpy as np
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import numpy as np
+import missingno as mno
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+from sklearn.impute import KNNImputer
+import time
+import torch
+from torch.optim.lr_scheduler import *
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.decomposition import PCA
+from sklearn.impute import KNNImputer
+from torchsummary import summary
+from torch.autograd import Variable
+from sklearn.metrics import *
+from torch.optim.lr_scheduler import ExponentialLR
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+from numpy.linalg import inv
+from numpy import linalg as LA
+import time
+
+
+# In[143]:
+"""
+wl_column='H'
+target_column= 'deplacement'
+
+methods_list=['std_regression',
+              'std_regression_cross',
+              'use_NN_optimizer',
+              'use_NN_optimizer_cross',
+             'use_bayesian_approch']
+optimizer='Adam'
+"""
+
+
+# In[144]:
+
+
+# checking nan values for data frame
+def check_nan_values(dataframe):
+    for column in dataframe.columns:
+        check=dataframe[column].isnull().values.any()
+        if check==True:
+            print(column, 'missing data: ',dataframe[column].isnull().sum())
+    else : print('dataframe on point')
+
+##time prep is computed for standard scaled time and for seasonal term whether it's used or not
+def time_prep(dataframe,column='Time'):
+    window_observation=dataframe[column][len(dataframe)-1]-dataframe[column][0]
+    dataframe['scaled_time']=dataframe[column].apply(lambda T :((T-dataframe[column][0]).total_seconds())/window_observation.total_seconds())
+    ## preaparing seasonal time for linear combination of sinusoidal functions
+    dataframe['seasonal_time']=dataframe[column].apply(lambda T :(2*np.pi*(T-dataframe[column][0]).total_seconds())/365*24*60*60)
+    return dataframe
+
+
+##hydrostatic load
+def hydrostatic_load(dataframe,wl_column,regression_order=4):
+    hydrostaic_columns=[]
+    max_observation=np.max(dataframe[wl_column])
+    min_observation=np.min(dataframe[wl_column])
+    scaled_observation=(max_observation-dataframe[wl_column])/(max_observation-min_observation)
+    dataframe['scaled_observation']=scaled_observation
+    for k in range(regression_order+1):
+        order='HL_'+str(k)
+        dataframe[order]=scaled_observation**k
+        hydrostaic_columns.append(order)
+    return dataframe,hydrostaic_columns
+#seasonal term
+
+def seasonal_term(dataframe):
+    #scaled_time=dataframe['seasonal_time']
+    season_columns=['sin','cos','sin2','sincos']
+    dataframe[season_columns[0]]=np.sin(dataframe['seasonal_time'])
+    dataframe[season_columns[1]]=np.cos(dataframe['seasonal_time'])
+    dataframe[season_columns[2]]=np.sin(dataframe['seasonal_time'])**2
+    dataframe[season_columns[3]]=np.sin(dataframe['seasonal_time'])*np.cos(dataframe['seasonal_time'])
+    return dataframe,season_columns
+
+#long term time influence
+
+
+def Time_influence(dataframe):
+    Time_columns=['T1','T2','T3']
+    dataframe[Time_columns[0]]=dataframe['scaled_time']
+    dataframe[Time_columns[1]]=np.exp(dataframe['scaled_time'])
+    dataframe[Time_columns[2]]=np.exp((-1)*dataframe['scaled_time'])
+    return dataframe , Time_columns
+
+"""
+Following "Hybrid GA/SIMPLS as alternative regression model in dam deformation analysis" paper
+the temperature componenent is computed conditionnaly on its observation existance. if it doesn't
+we compute a sum of sin and cos based on seasonal time
+"""
+
+def temperature_component(data,temp_column):
+    if temp_column is not None :
+        scale=np.max(data[temp_column])-np.min(data[temp_column])
+        data['temperature']=data[temp_column]-np.mean(data[temp_column])
+        data['temperature']/=scale
+        return data,['temperature']
+    else:
+        Temp_columns=[]
+        for k in range(1,6):
+            data['sin_temp_'+str(k)]=np.sin(k*data['seasonal_time'])
+            data['cos_temp_'+str(k)]=np.cos(k*data['seasonal_time'])
+            Temp_columns.append('sin_temp_'+str(k),'cos_temp_'+str(k))
+    return data ,Temp_columns
+
+def variable_component(data,variable_column):
+    scale=np.max(data[variable_column])-np.min(data[variable_column])
+    if scale==0:
+        data[variable_column]=data[variable_column]-np.mean(data[variable_column])
+    else:
+        data[variable_column]=data[variable_column]-np.mean(data[variable_column])
+        data[variable_column]/=scale
+    return data , [variable_column]
+
+
+
+def prepare_prediction(dataframe,model):
+    #assert len(model_columns)==len(model)
+    df=dataframe.copy()
+    df=time_prep(df)
+    kept_columns=[]
+    static_colums=['H','S','T','temp']
+    if 'H' in model :
+        df,columns=hydrostatic_load(df,'water_level',regression_order=4)
+        kept_columns+=columns
+    if 'S' in model:
+        df,columns=seasonal_term(df)
+        kept_columns+=columns
+    if 'T' in model :
+        df,columns=Time_influence(df)
+        kept_columns+=columns
+    if 'temp' in model :
+        df,columns=temperature_component(df,'temperature')
+        kept_columns+=columns
+
+    added_variables=list(set(model)-set(np.intersect1d(static_colums,model)))
+    if len(added_variables)>0 :
+        for i in range(len(added_variables)):
+            df,columns=variable_component(df,added_variables[i])
+            kept_columns+=columns
+    return df[kept_columns]
+
+
+
+
+
+
+def linear_regression(data,target):
+    #monitored_=data[pd.notnull(data[target_column])]
+    #monitored_=monitored_[pd.notnull(monitored_[wl_column])]
+    start=time.time()
+    #dataframe=prepare_prediction(data,model)
+    #print('computed variables in :' ,time.time()-start)
+    #check_nan_values(data)
+    regr =LinearRegression()
+    #regr.fit(dataframe,monitored_[target_column])
+    regr.fit(data,target)
+    #print( 'regr score for '+target_column+'is:' , regr.score(data,target))
+    #dataframe.insert(0,'corrected observation',monitored_[target_column] -np.array(regr.predict(dataframe)))
+    #dataframe.insert(len(dataframe.columns),target_column,monitored_[target_column])
+    return regr
+
+
+# In[70]:
+
+
+def regression_kfold(matrix,target,n_splits):
+    cv_outer = KFold(n_splits, shuffle=True, random_state=0)
+    # enumerate splits
+    outer_results = list()
+    X=matrix.T
+    y=target
+    best_score=np.NINF
+    best_regr=0,0
+    for train_ix, test_ix in cv_outer.split(X):
+        X_train, X_test = X[train_ix, :], X[test_ix, :]
+        y_train, y_test = y[train_ix], y[test_ix]
+        #cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+        #__,weights,biais=optimizer_variation(X_train.T,  y_train,  'Adam',  lr=0.1,  num_epoch=2500)
+        regr=linear_regression(X_train,y_train)
+        y_hat=regr.predict(X_test)
+        score=r2_score(y_hat,y_test)
+        """
+        y_hat=weights.mm(torch.Tensor(X_test.T)) + biais
+        y_hat=y_hat.squeeze()
+        score=r2_score(y_test,y_hat.detach().numpy())"""
+
+        if score>best_score :
+            best_score=score
+            best_regr=regr
+    #print(f"final score : {score :.3f}")
+    return best_regr
+
+
+
+def standard_regression(data,target,use_cross_val):
+    if use_cross_val:
+        regr=regression_kfold(data.T,target,n_splits=3)
+        y_hat=regr.predict(data)
+        ##print( 'regr score for '+target_column+'is:' , regr.score(dataframe,monitored_[target_column]))
+        #dataframe.insert(0,'corrected observation',monitored_[target_column] -np.array(regr.predict(dataframe)))
+        #dataframe.insert(len(dataframe.columns),target_column,monitored_[target_column])
+    else:
+        regr=linear_regression(data,target)
+        y_hat=regr.predict(data)
+    return y_hat
+
+
+
+# Main optimization loop
+def optimizer_variation(matrix,  target,  optimizer,  lr=0.01,  num_epoch=2500):
+    A = torch.randn((1, matrix.shape[0]), requires_grad=True)
+    b = torch.randn(1, requires_grad=True)
+    if optimizer=='SGD':
+        opt = torch.optim.SGD([A, b], lr=lr)
+    elif optimizer=='Adam' :
+        opt=torch.optim.Adam([A, b], lr=lr)
+    elif optimizer=='AdamW':
+        opt=torch.optim.AdamW([A, b], lr=lr)
+    elif optimizer=='Adamax':
+        opt=torch.optim.AdamW([A, b], lr=lr)
+    elif optimizer=='ASGD':
+        opt=torch.optim.ASGD([A, b], lr=lr)
+    elif optimizer=='Adagrad':
+        opt=torch.optim.Adagrad([A, b], lr=lr)
+    ##setting up a learning rate scheduler
+    criterion=torch.nn.MSELoss()
+    scheduler = ExponentialLR(opt, gamma=0.1)
+    ##Multi Linear type model
+    def NN(x_input):
+        return A.mm(x_input) + b
+    best_score=np.NINF
+    best_loss=np.inf
+    best_pred=0
+    ##Trainning loop
+    for t in range(0,num_epoch):
+        opt.zero_grad()
+        y_predicted = NN(torch.Tensor(matrix))
+        current_loss = criterion(y_predicted, torch.Tensor(target))
+        current_loss.backward()
+        opt.step()
+        pred=y_predicted.detach().numpy().squeeze()
+        score=r2_score(target,pred)
+        ##keeping best model
+        if score>best_score :
+            best_score=score
+            best_pred=target-pred
+            best_loss=current_loss
+            weights=A
+            biais=b
+    #print(f"t = {t}, loss: {best_loss:.2F}, score : {score :.3f}")
+    prediction=NN(torch.Tensor(matrix))
+    return prediction,weights,biais
+
+
+# create dataset
+# configure the cross-validation procedure
+def k_fold_cross(matrix,target,optimizer):
+    cv_outer = KFold(n_splits=6, shuffle=True, random_state=0)
+    # enumerate splits
+    outer_results = list()
+    X=matrix.T
+    y=target
+    best_score=np.NINF
+    best_weights,best_biais=0,0
+    for train_ix, test_ix in cv_outer.split(X):
+        X_train, X_test = X[train_ix, :], X[test_ix, :]
+        y_train, y_test = y[train_ix], y[test_ix]
+        #cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+        __,weights,biais=optimizer_variation(X_train.T,  y_train,  optimizer,  lr=0.1,  num_epoch=1000)
+        y_hat=weights.mm(torch.Tensor(X_test.T)) + biais
+        y_hat=y_hat.squeeze()
+        score=r2_score(y_test,y_hat.detach().numpy())
+
+        if score>best_score :
+            best_score=score
+            best_weights=weights
+            best_biais=biais
+    #print(f"final score : {score :.3f}")
+    final_pred=weights.mm(torch.Tensor(matrix)) + biais
+    return final_pred
+
+
+# In[220]:
+
+
+def calculate_w(l, phi, t, N_rows, M_cols):
+    LI = np.eye(M_cols) * l
+    innerPrdt = (LI + np.matmul(np.transpose(phi),phi))
+    w = np.matmul(inv(innerPrdt),np.matmul(np.transpose(phi),t))
+    return w
+def bayesian_model_selection(train, trainR, test, testR):
+    start_time = time.time()
+    alpha = 2.34
+    beta = 3.22
+    prev_alpha = 0
+    prev_beta = 0
+    N_rows_train = np.shape(train)[0]
+    M_features_train = np.shape(train)[1]
+    N_rows_test = np.shape(test)[0]
+    i = 0
+    while abs(prev_alpha-alpha) > 0.0001 and abs(prev_beta-beta) > 0.0001:
+        prev_alpha = alpha
+        prev_beta = beta
+        eigen_phiT_phi = LA.eigvals(np.matmul(np.transpose(train),train))
+        Sn_inv = alpha * np.eye(M_features_train) + beta * np.matmul(np.transpose(train),train)
+        Mn = beta * np.matmul(inv(Sn_inv),np.matmul(np.transpose(train),trainR))
+        lamda = beta * eigen_phiT_phi
+        alpha_lamda = LA.eigvals(Sn_inv)
+        gamma = sum(lamda/alpha_lamda)
+        alpha = gamma / np.matmul(np.transpose(Mn),Mn)
+        beta = 1/(sum((trainR - np.matmul(train,Mn)) ** 2) / (N_rows_train-gamma))
+        i+=1
+
+    w = calculate_w(alpha/beta,train,trainR,N_rows_train,M_features_train)
+    mse = (sum((np.matmul(test,w) - testR) ** 2) / N_rows_test)
+    #print("computed bayesian model in ", time.time()-start_time)
+    return mse,w
+
+def bayesian_selection(matrix,target,n_splits=2):
+    X=matrix.T
+    y=target
+
+    if n_splits==1 :
+        X_train, y_train, X_test, y_test=train_test_split(X,y,test_size = 0.25,shuffle=False)
+        return bayesian_model_selection(X_train, y_train, X_test, y_test)
+    else:
+        cv_outer = KFold(n_splits=n_splits, shuffle=True, random_state=0)
+            # enumerate splits
+
+        best_score=np.NINF
+        best_weights,best_biais=0,0
+        best_score=np.NINF
+        best_weight=0
+        for train_ix, test_ix in cv_outer.split(X):
+            X_train, X_test = X[train_ix, :], X[test_ix, :]
+            y_train, y_test = y[train_ix], y[test_ix]
+            mse,weight=bayesian_model_selection(X_train, y_train, X_test, y_test)
+            if mse>best_score :
+                best_score=mse
+                best_weight=weight
+        best_weight=best_weight.reshape(best_weight.shape[0])
+    return np.dot(X,best_weight)
+
+
+# In[221]:
+
+
+
+
+# In[222]:
+
+def deploy(model,data,target_column,method,optimizer=None):
+    methods_list=['std_regression',
+              'std_regression_cross',
+              'use_NN_optimizer',
+              'use_NN_optimizer_cross',
+             'use_bayesian_approch']
+    optimizers_list=['SGD','Adam', 'AdamW','Adamax','ASGD','Adagrad']
+    #assert method in methods_list
+    matrix=np.array(prepare_prediction(data,model)).T
+    target=np.array(data[target_column])
+    dataframe=data.copy()
+    if method==methods_list[0]:
+
+        corrected_obs=standard_regression(matrix.T,target,use_cross_val=False)
+        dataframe.insert(0,'corrected observation',data[target_column] -np.array(corrected_obs))
+    elif  method==methods_list[1]:
+        corrected_obs=standard_regression(np.transpose(matrix),target,use_cross_val=True)
+        dataframe.insert(0,'corrected observation',data[target_column] -np.array(corrected_obs))
+    elif  method==methods_list[2]:
+        if optimizer is not None :
+            assert optimizer in optimizers_list
+            corrected_obs,__,__=optimizer_variation(matrix,  target,  optimizer,  lr=0.1,  num_epoch=2500)
+            dataframe.insert(0,'corrected observation',data[target_column] -corrected_obs.squeeze().detach().numpy())
+        else:
+            corrected_obs,__,__=optimizer_variation(matrix,  target,  optimizer='SGD',  lr=0.1,  num_epoch=2500)
+            dataframe.insert(0,'corrected observation',data[target_column] -corrected_obs.squeeze().detach().numpy())
+    elif method==methods_list[3]:
+        if optimizer is not None :
+            assert optimizer in optimizers_list
+            corrected_obs=k_fold_cross(matrix,target,optimizer)
+            dataframe.insert(0,'corrected observation',data[target_column] -corrected_obs.squeeze().detach().numpy())
+        else:
+            corrected_obs=k_fold_cross(matrix,target,optimizer='SGD')
+            dataframe.insert(0,'corrected observation',data[target_column] -corrected_obs.squeeze().detach().numpy())
+    elif method==methods_list[4]:
+        corrected_obs=bayesian_selection(matrix,target,n_splits=2)
+        dataframe.insert(0,'corrected observation',data[target_column] -np.array(corrected_obs))
+    #print(dataframe.shape)
+    return dataframe
+
+
+##some helper functions
+def normalize_date_time(dataframes):
+    for data in dataframes:
+        data['timestamp'] = pd.to_datetime(data['timestamp'].astype('datetime64[ns]'))
+    return dataframes
+
+def time_interpolation(d1,d2):
+    df=pd.concat([d1, d2])
+    dataframes=pd.DataFrame(df['timestamp'])
+    dataframes.set_index(df['timestamp'])
+    df=df.set_index('timestamp')
+    df = df.sort_values(by="timestamp")
+    for column in df.columns:
+        d1=df[column]
+        dataframes[column]=pd.DataFrame(d1.interpolate()).values
+    return dataframes
+def datetime_interploation(dataframes):
+    df=dataframes[0]
+    for data in dataframes[1:]:
+        df=time_interpolation(df,data)
+    return df
+
+
+def final_prep(dataframes):
+    target_column='target_variable'
+    dataframes=normalize_date_time(dataframes)
+    df=datetime_interploation(dataframes)
+    df=df.dropna()
+    df = df.rename(index=str,columns= {'timestamp': "Time"})
+    df['Time'] = pd.to_datetime(df['Time'])
+    #model_columns=list(df.columns)
+    #model_columns.remove(target_column)
+    #return df,model_columns
+    return df
+
+
+def prepare_corrected_obs(res,id_res_var):
+    df_res  = res[['Time','corrected observation']]
+    df_res=df_res.rename(index=str,columns= {'Time': "timestamp"})
+    df_res=df_res.rename(index=str,columns= {'corrected observation': "value"})
+    df_res['variable_id'] = id_res_var
+    return df_res
+
+
+
+
+
+
+
+
+# In[223]:
+
+
+
+
+
+# In[ ]:
---
+++ database.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import psycopg2
+from io import StringIO
+import os
+import pandas as pd
+import json
+import numpy as np
+class Database:
+    def __init__(self, host, database, user, password, port):
+        self.password = password
+        self.host = host
+        self.db_name = database
+        self.user = user
+        self.port = port
+        print("Database Ready")
+    def getConnection(self):
+        conn = psycopg2.connect(host=self.host, dbname=self.db_name, user=self.user, password=self.password, port=self.port)
+        return conn
+    def selectDataframe(self, query):
+        conn = None
+        try:
+            conn = self.getConnection()
+            df = pd.read_sql_query(query, con=conn)
+            conn.close()
+            return df
+        except psycopg2.DatabaseError as error:
+            raise error
+        finally:
+            if conn is not None:
+                conn.close()
+    def select(self, query, parameter=[]):
+
+        conn = psycopg2.connect(
+            host=self.host,
+            dbname=self.db_name,
+            user=self.user,
+            password=self.password,
+            port=self.port)
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query, parameter)
+        except:
+            print("cant execute query")
+            raise
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+    def delete(self, query):
+        conn = None
+        try:
+            conn = self.getConnection()
+            cursor = conn.cursor()
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+        except psycopg2.DatabaseError as error:
+            raise error
+        finally:
+            if conn is not None:
+                conn.close()
+    def insert(self, query, parameter=[]):
+        conn = psycopg2.connect(
+            host=self.host,
+            dbname=self.db_name,
+            user=self.user,
+            password=self.password,
+            port=self.port)
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query, parameter)
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cursor.close()
+            conn.close()
+            return 1
+        cursor.close()
+        conn.close()
+    def insert_returning_id(self, query, parameter=[]):
+        conn = psycopg2.connect(
+            host=self.host,
+            dbname=self.db_name,
+            user=self.user,
+            password=self.password,
+            port=self.port)
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query, parameter)
+            id_returning = cursor.fetchone()[0]
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cursor.close()
+            conn.close()
+            return -1
+        cursor.close()
+        conn.close()
+        return id_returning
+
+    def copy_from_df(self, df):
+        conn = psycopg2.connect(
+            host=self.host,
+            dbname=self.db_name,
+            user=self.user,
+            password=self.password,
+            port=self.port )
+        cur = conn.cursor()
+        s_buf = io.StringIO()
+        df.to_csv(s_buf, header=False, index=True)
+        # adapt_row is a custom method, differing from psyopg's adapt
+        s_buf.seek(0)
+        cur.copy_from(
+         s_buf, "raw_data", sep=",", columns=("timestamp", "variable_id", "value") )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    def insertDataframe(self, df, table):
+        # save dataframe to an in memory buffer
+        my_df = df.set_index('timestamp')
+        buffer = StringIO()
+        my_df.to_csv(buffer, index_label='id', header=False)
+        buffer.seek(0)
+
+        conn = self.getConnection()
+        cursor = conn.cursor()
+        #try:
+        cursor.copy_from(buffer, table, sep=",", columns=("timestamp", "value", "variable_id"))
+        conn.commit()
+        #except (Exception, psycopg2.DatabaseError) as error:
+        #    print("Error: %s" % error)
+        #    conn.rollback()
+    #        cursor.close()
+            #return 1
+        print("Insert dataframe done with success!")
+        cursor.close()
+
+
+    def toPandas(self, query, params = None):
+        conn = None
+        try:
+            conn = self.getConnection()
+            data = pd.read_sql(query, conn, params=params)
+            return data
+        except psycopg2.DatabaseError as error:
+            raise error
+        finally:
+            if conn is not None:
+                conn.close()
+class Query:
+    def __init__(self, host, db_name, user, port, password):
+        self.db = Database(
+            host=host, database=db_name, user=user, port=port, password=password )
+    def get_df_by_variable_id(self, variable_id, variable_name):
+        query = """
+            SELECT r.timestamp, r.value, r.variable_id FROM raw_data r
+            WHERE r.variable_id = %s;
+        """
+        data = self.db.select(query, (variable_id,))
+        df = pd.DataFrame(data=data, columns=["timestamp", variable_name,'id'])
+        return df.drop(columns=['id'])
+    def get_df_by_variable_id(self, variable_id, variable_name):
+        query = """
+            SELECT r.timestamp, r.value, r.variable_id FROM raw_data r
+            WHERE r.variable_id = %s;
+        """
+        data = self.db.select(query, (variable_id,))
+        df = pd.DataFrame(data=data, columns=["timestamp", variable_name,'id'])
+        return df.drop(columns=['id'])
+    def get_variable_name_by_id(self, variable_id):
+
+        query="""
+        select  variable_id,name from raw_data rd
+        join variables v on rd.variable_id=v.id
+        WHERE variable_id={0}""".format(variable_id)
+
+
+        data = self.db.selectDataframe(query)
+        return data.iloc[0][1]
+    def deleteData(self, offset_variable_id, startDate):
+        sqlQuery = """
+                    DELETE FROM raw_data_save
+                    WHERE timestamp >= to_timestamp('{}','YYYY-MM-DD HH24:MI:SS') and variable_id={}
+                    """.format(startDate, offset_variable_id)
+        self.db.delete(sqlQuery)
+
+    def insert_result_variable(self, df, res_name, res_metric_id) :
+        # create new sensor for the resulted variable
+        query = """
+                INSERT INTO sensors(name,sensor_type_id,x,y,z,installation_date,initial_frequency,site_gateway_id) values(%s,%s,%s,%s,%s,%s,%s,%s) RETURNING id;
+            """
+        parameter = (res_name,12,1650233,8392540,100,'04/03/2021',20,1)
+        sensor_id = self.db.insert_returning_id(query, parameter)
+        # print('sensor_id'+ str(sensor_id))
+        # creating variables in db for the created sensors
+        query = """
+                INSERT INTO variables (name,metric_id,sensor_id) values(%s,%s,%s) RETURNING id;
+            """
+        parameter = (res_name, res_metric_id, sensor_id)
+        variable_id = self.db.insert_returning_id(query, parameter)
+        # inserting res value to db (raw_data table)
+        for i in range (0,len(df)) :
+            query = """
+                INSERT INTO raw_data (timestamp, value, variable_id) values(%s,%s,%s);
+            """
+            parameter =  (df.iloc[i]['timestamp'], df.iloc[i]['res'][0], variable_id)
+            self.db.insert(query, parameter)
+
+        return variable_id, sensor_id
